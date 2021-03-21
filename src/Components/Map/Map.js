77b590b7c468873911d258e39dc8e1b431f3c166
Tanjin <tanjin.ahamed123@gmail.com>
2021-03-21 17:59:32 +0600

    map done

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,87 +1,34 @@
-// import React, { useState, useEffect } from "react";
-// import {
-//   withGoogleMap,
-//   withScriptjs,
-//   GoogleMap,
-//   Marker,
-//   InfoWindow
-// } from "react-google-maps";
+// import React from 'react'
+// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+// // import {API_KEY} from './config';
+// const containerStyle = {
+//   width: '400px',
+//   height: '400px',
+ 
+// };
 
-// import * as parkData from "../Data/skateboard-parks.json";
-// import mapStyles from "./mapStyles.css";
+// const center = {
+//   lat: 23.71573,
+//   lng: 90.38395
+// };
 
 // function Map() {
-//   const [selectedPark, setSelectedPark] = useState(null);
-
-//   useEffect(() => {
-//     const listener = e => {
-//       if (e.key === "Escape") {
-//         setSelectedPark(null);
-//       }
-//     };
-//     window.addEventListener("keydown", listener);
-
-//     return () => {
-//       window.removeEventListener("keydown", listener);
-//     };
-//   }, []);
-
 //   return (
-//     <GoogleMap
-//       defaultZoom={10}
-//       defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
-//       defaultOptions={{ styles: mapStyles }}
+//     <LoadScript
+//       googleMapsApiKey="YOUR_API_KEY"
 //     >
-//       {parkData.features.map(park => (
-//         <Marker
-//           key={park.properties.PARK_ID}
-//           position={{
-//             lat: park.geometry.coordinates[1],
-//             lng: park.geometry.coordinates[0]
-//           }}
-//           onClick={() => {
-//             setSelectedPark(park);
-//           }}
-//           icon={{
-//             url: `/skateboarding.svg`,
-//             scaledSize: new window.google.maps.Size(25, 25)
-//           }}
-//         />
-//       ))}
-
-//       {selectedPark && (
-//         <InfoWindow
-//           onCloseClick={() => {
-//             setSelectedPark(null);
-//           }}
-//           position={{
-//             lat: selectedPark.geometry.coordinates[1],
-//             lng: selectedPark.geometry.coordinates[0]
-//           }}
-//         >
-//           <div>
-//             <h2>{selectedPark.properties.NAME}</h2>
-//             <p>{selectedPark.properties.DESCRIPTIO}</p>
-//           </div>
-//         </InfoWindow>
-//       )}
-//     </GoogleMap>
-//   );
+//       <GoogleMap
+//         mapContainerStyle={containerStyle}
+//         center={center}
+//         zoom={10}
+//       >
+//         <Marker      
+//       position={center}
+//     />
+    
+//       </GoogleMap>
+//     </LoadScript>
+//   )
 // }
 
-// const MapWrapped = withScriptjs(withGoogleMap(Map));
-
-// export default function App() {
-//   return (
-//     <div style={{ width: "100vw", height: "100vh" }}>
-//       <MapWrapped
-//         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-//           process.env.REACT_APP_GOOGLE_KEY
-//         }`}
-//         loadingElement={<div style={{ height: `100%` }} />}
-//         containerElement={<div style={{ height: `100%` }} />}
-//         mapElement={<div style={{ height: `100%` }} />}
-//       />
-//     </div>
-//   );
-// }
+// export default React.memo(Map)
--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -2,10 +2,9 @@ import React, { useContext, useState } from 'react';
 import './SignIn.css';
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router';
-import { createUserWithEmailAndPassword, SignInUserWithEmailAndPassword, gitHubSignIn, googleSignIn, initializeSignIn, updateUserInfo }  from './signInManager';
+import { createUserWithEmailAndPassword, SignInUserWithEmailAndPassword, gitHubSignIn, googleSignIn, initializeSignIn, updateUserInfo } from './signInManager';
 
-const SignIn = () =>
-{
+const SignIn = () => {
     initializeSignIn();
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const [newUser, setNewUser] = useState(false);
@@ -19,55 +18,47 @@ const SignIn = () =>
         success: false
     });
 
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState('');
+
     const history = useHistory();
     const location = useLocation();
     const { from } = location.state || { from: { pathname: "/" } };
 
-    const handleResponse = (res, redirect) =>
-    {
+    const handleResponse = (res, redirect) => {
         setUser(res)
         setLoggedInUser(res)
-        if (redirect)
-        {
+        if (redirect) {
             history.replace(from);
         }
     }
 
-    const handleGoogleSignIn = () =>
-    {
+    const handleGoogleSignIn = () => {
         googleSignIn()
-            .then(res =>
-            {
+            .then(res => {
                 handleResponse(res, true)
             })
     }
 
-    const handleGitHubSignIn = () =>
-    {
+    const handleGitHubSignIn = () => {
         gitHubSignIn()
-            .then(res =>
-            {
+            .then(res => {
                 handleResponse(res, true);
                 setLoggedInUser(res);
             })
     }
 
-    const handleSubmit = (event) =>
-    {
-        if (newUser && user.name && user.email && user.password)
-        {
+    const handleSubmit = (event) => {
+        if (newUser && user.name && user.email && user.password) {
             createUserWithEmailAndPassword(user.name, user.email, user.password)
-                .then(res =>
-                {
+                .then(res => {
                     handleResponse(res, true);
                     updateUserInfo(user.name);
                 })
         }
-        if (!newUser && user.email && user.password)
-        {
+        if (!newUser && user.email && user.password) {
             SignInUserWithEmailAndPassword(user.email, user.password)
-                .then(res =>
-                {
+                .then(res => {
                     handleResponse(res, true);
                     setLoggedInUser(res);
                 })
@@ -75,29 +66,39 @@ const SignIn = () =>
         event.preventDefault();
     }
 
-    const handleBlur = (event) =>
-    {
+    const handleBlur = (event) => {
         let isFormValid;
 
-        if (event.target.name === 'name')
-        {
+        if (event.target.name === 'name') {
             isFormValid = event.target.value.length > 4;
         }
 
-        if (event.target.name === 'email')
-        {
+        if (event.target.name === 'email') {
             isFormValid = /\S+@\S+\.\S+/.test(event.target.value);
         }
 
-        if (event.target.name === 'password')
-        {
+        if (event.target.name === 'password') {
             const isPasswordValid = event.target.value.length > 6;
             const isPasswordHasNumber = /\d{1}/.test(event.target.value);
+
+            if (isPasswordValid && isPasswordHasNumber) {
+                setPassword(event.target.value)
+            }
             isFormValid = isPasswordValid && isPasswordHasNumber;
         }
 
-        if (isFormValid)
-        {
+
+        if (event.target.name === 'ConfirmPassword') {
+            const isPasswordValid = event.target.value.length > 6;
+            const isPasswordHasNumber = /\d{1}/.test(event.target.value);
+
+            if (isPasswordValid && isPasswordHasNumber) {
+                setConfirmPassword(event.target.value)
+            }
+            isFormValid = isPasswordValid && isPasswordHasNumber;
+        }
+
+        if (isFormValid) {
             const newUserInfo = { ...user };
             newUserInfo[event.target.name] = event.target.value;
             setUser(newUserInfo);
@@ -117,8 +118,17 @@ const SignIn = () =>
                 }
                 <label htmlFor="inputEmail" className="visually-hidden">Email address</label>
                 <input type="email" onBlur={handleBlur} name="email" className="form-control" placeholder="Email address" required autoFocus />
-                <label htmlFor="inputPassword" className="visually-hidden">Password</label>
-                <input type="password" onBlur={handleBlur} name="password" className="form-control" placeholder="Password" required />
+
+                {
+                    newUser ? <div>
+                        <label htmlFor="inputPassword" className="visually-hidden">Password</label>
+                        <input type="password" onBlur={handleBlur} name="password" className="form-control" placeholder="Password" required />
+                        <label htmlFor="inputPassword" className="visually-hidden">Confirm Password</label>
+                        <input type="password" onBlur={handleBlur} name="ConfirmPassword" className="form-control" placeholder="Confirm Password" required />
+
+                    </div> : <div><label htmlFor="inputPassword" className="visually-hidden">Password</label>
+                        <input type="password" onBlur={handleBlur} name="password" className="form-control" placeholder="Password" required /></div>
+                }
                 <button className="w-100 btn btn-lg btn-outline-success mb-2" type="submit">{newUser ? 'Create an account' : 'Signin'}</button>
                 <p>{newUser ? 'Already have an account' : 'Don’t have an account'} ?
                  <span onClick={() => setNewUser(!newUser)} className="text-warning" style={{ cursor: 'pointer' }}>
